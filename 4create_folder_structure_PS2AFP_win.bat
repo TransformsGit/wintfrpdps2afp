b2afa680bc95e39f6c8d89fcec5b9cc70af4e75a
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-21 10:56:10 +0300

    Add files via upload

---
+++ 4create_folder_structure_PS2AFP_win.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+
+Set dir=E:\OutputTransformPS2AFP\win\output
+Echo Deleting all files from %dir%
+del %dir%\* /F /Q
+
+Echo Deleting all folders from %dir%
+for /d %%p in (%dir%\*) Do rd /Q /S "%%p"
+@echo Folder deleted.
+
+
+mkdir E:\OutputTransformPS2AFP\win\output\out_AFP\TestFiles
+echo mkdir E:\OutputTransformPS2AFP\win\output\out_AFP\TestFiles
+
+mkdir E:\OutputTransformPS2AFP\win\output\out_PDF\TestFiles
+echo mkdir E:\OutputTransformPS2AFP\win\output\out_PDF\TestFiles
+
+mkdir E:\OutputTransformPS2AFP\win\output\out_TIFF\MasterFiles
+echo mkdir E:\OutputTransformPS2AFP\win\output\out_TIFF\MasterFiles
+
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPS2AFP\win\output\out_AFP\TestFiles\ /s /i
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPS2AFP\win\output\out_PDF\TestFiles\ /s /i
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPS2AFP\win\output\out_TIFF\MasterFiles\ /s /i
+
+
+echo Folder structure succesfully done!
